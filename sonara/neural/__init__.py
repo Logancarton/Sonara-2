a04197dfe766aac738ff84ea362392ef253f5b54
Logancarton <logan_1928@msn.com>
2026-09-05 13:17:00 -0700

    Add neural package exports

---
+++ sonara/neural/__init__.py
@@ -0,0 +1,11 @@
+from .population import NeuronPopulation, PopulationParams
+from .projection import SynapseProjection
+from .network import PropagationNetwork, TickResult
+
+__all__ = [
+    "NeuronPopulation",
+    "PopulationParams",
+    "SynapseProjection",
+    "PropagationNetwork",
+    "TickResult",
+]
